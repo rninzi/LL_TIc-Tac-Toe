27ab669905c2ccd46b3d339281c0c44a8c573d65
mywhzy <cmj2393a@naver.com>
2023-01-13 01:13:25 +0900

    feat/ show board & draw marker

--- src/components/Block.js
+++ src/components/Block.js
@@ -1,18 +1,29 @@
 // borad의 구성요소가 되는 한 칸(블럭)
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
-const BlockBtn = styled.button`
+const BlockBtn = styled.div`
   background-color: white;
   border: 1px solid black;
   width: 100px;
   height: 100px;
   font-size: 50px;
+  text-align: center;
+  line-height: 100px;
 `;
-const Block = () => {
+const Block = ({ playerMarker, changePlayer }) => {
+  const [marker, setMarker] = useState("");
+
+  const onBlockClick = () => {
+    if (marker === "") {
+      // 블럭 마커 표시 여부 판별 (중복 방지)
+      setMarker(playerMarker);
+      changePlayer();
+    }
+  };
   return (
     <>
-      <BlockBtn></BlockBtn>
+      <BlockBtn onClick={onBlockClick}>{marker}</BlockBtn>
     </>
   );
 };
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,11 +1,44 @@
 //틱택토 게임보드 (블럭의 집합)
-import React from "react";
+import React, { useState } from "react";
 import Block from "./Block";
+import styled from "styled-components";
+
+const PlayInfo = styled.h3`
+  color: black;
+`;
+
+const BoardTopBox = styled.div`
+  display: flex;
+`;
+const BoardRow = styled.div``;
 
 const Board = () => {
+  const [id, setId] = useState(1);
+  const [marker, setMarker] = useState("X");
+  const changePlayer = () => {
+    if (id === 1) {
+      setId(2);
+      setMarker("O");
+    } else {
+      setId(1);
+      setMarker("X");
+    }
+  };
+  const gameBoard = Array.from(Array(3), () =>
+    Array(3).fill(
+      <Block playerId={id} playerMarker={marker} changePlayer={changePlayer} />
+    )
+  );
+
+  const showBoard = gameBoard.map((item) => <BoardRow>{item}</BoardRow>);
+
   return (
     <>
-      <Block />
+      <PlayInfo>
+        player1:"X" player2:"O" <br />
+        현재 플레이어: player{id}
+      </PlayInfo>
+      <BoardTopBox>{showBoard}</BoardTopBox>
     </>
   );
 };
